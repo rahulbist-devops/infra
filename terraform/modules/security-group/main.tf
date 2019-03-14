d47641678922dee9c3f565941e7eebf3d77d1c29
rahulbist-devops <rahul.bist@monotype.com>
2019-03-14 10:47:48 +0530

    Update main.tf

--- terraform/modules/security-group/main.tf
+++ terraform/modules/security-group/main.tf
@@ -1,6 +1,20 @@
 resource "aws_security_group" "security-group" {
   name = "${var.security-group-name}"
   vpc_id = "${var.vpc-id}"
+
+ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["${var.cidr}"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
 }
 
 output "security-group-id" {
