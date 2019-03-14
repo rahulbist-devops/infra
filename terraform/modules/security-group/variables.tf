e41803517565a643eccdd1354bb6facc3cf57921
rahulbist-devops <rahul.bist@monotype.com>
2019-03-14 10:48:04 +0530

    Update variables.tf

--- terraform/modules/security-group/variables.tf
+++ terraform/modules/security-group/variables.tf
@@ -5,3 +5,7 @@ variable "security-group-name" {
 variable "vpc-id" {
   description = "ID of the VPC to create the security group in"
 }
+
+variable "cidr" {
+  description = "cidr range to be whitelisted"
+}
